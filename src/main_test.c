1c0db2a05a1ec960317d5458afaf3ad0ae7feebc
Trichet Pierre <pierre42@MacBook-Pro-de-Benoit.local>
2021-04-16 17:23:58 +0200

    added Total line on show_mem_alloc, and fully fixed main

--- src/main_test.c
+++ src/main_test.c
@@ -6,7 +6,7 @@
 /*   By: pierre42 <pierre42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 17:24:44 by pitriche          #+#    #+#             */
-/*   Updated: 2020/07/09 19:00:06 by pierre42         ###   ########.fr       */
+/*   Updated: 2021/04/16 17:19:57 by pierre42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int		main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	t_malloc *m = malloc_data();
 
 	void *p1 = ft_malloc(1 * 8000);
 	void *p2 = ft_malloc(2 * 8000);
@@ -26,7 +25,7 @@ int		main(int ac, char **av)
 	void *p5 = ft_malloc(5 * 8);
 	show_alloc_mem();
 	ft_strcpy(p5, "lololol itz funi");
-	printf(">%s<\n", p5);
+	ft_printf(">%s<\n", p5);
 	ft_strcpy(p5, "lololol itz funi and it ovarflow lalalalalalallalalalla");
 
 	show_alloc_mem();
@@ -34,9 +33,9 @@ int		main(int ac, char **av)
 	ft_free(p3);
 	ft_free(p1);
 	ft_free(p2);
-	printf(">%p<\n", p5);
-	//p5 = ft_realloc(p5, 1200);
-	printf(">%p<\n", p5);
+	ft_printf(">%p<\n", p5);
+	p5 = ft_realloc(p5, 1200);
+	ft_printf(">%p<\n", p5);
 	ft_free(p5);
 	show_alloc_mem();
 	return (0);
--- src/show_alloc_mem.c
+++ src/show_alloc_mem.c
@@ -6,72 +6,93 @@
 /*   By: pierre42 <pierre42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/02 18:19:22 by pitriche          #+#    #+#             */
-/*   Updated: 2020/07/08 19:08:05 by pierre42         ###   ########.fr       */
+/*   Updated: 2021/04/16 17:18:21 by pierre42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
 #include "libft.h"
 
-static void	pr_tiny(t_malloc *d)
+static unsigned long	pr_tiny(t_malloc *d)
 {
-	t_ma_tiny	*tmpt;
-	unsigned	i;
+	t_ma_tiny		*tmpt;
+	unsigned		i;
+	unsigned long	total;
 
 	tmpt = d->tiny;
+	total = 0UL;
 	ft_printf("TINY : 0x%X\n", tmpt);
 	while (tmpt)
 	{
 		i = -1;
 		while(++i < tmpt->n_elem)
+		{
 			ft_printf("0x%X - 0x%X : %lu octets\n", tmpt->mem +
 				tmpt->meta[i].off, tmpt->mem + tmpt->meta[i].off +
 				tmpt->meta[i].len, tmpt->meta[i].len);
+			total += tmpt->meta[i].len;
+		}
 		tmpt = tmpt->next;
 	}
+	return (total);
 }
 
-static void	pr_small(t_malloc *d)
+static unsigned long	pr_small(t_malloc *d)
 {
-	t_ma_smal	*tmps;
-	unsigned	i;
+	t_ma_smal		*tmps;
+	unsigned		i;
+	unsigned long	total;
 
 	tmps = d->smal;
+	total = 0UL;
 	ft_printf("SMALL : 0x%X\n", tmps);
 	while (tmps)
 	{
 		i = -1;
 		while(++i < tmps->n_elem)
+		{
 			ft_printf("0x%X - 0x%X : %lu octets\n", tmps->mem +
 				tmps->meta[i].off, tmps->mem + tmps->meta[i].off +
 				tmps->meta[i].len, tmps->meta[i].len);
+			total += tmps->meta[i].len;
+		}
 		tmps = tmps->next;
 	}
+	return (total);
 }
 
-static void	pr_large(t_malloc *d)
+static unsigned long	pr_large(t_malloc *d)
 {
 	t_ma_larg	*tmpl;
 	unsigned	i;
+	unsigned long	total;
 
 	tmpl = d->larg;
+	total = 0UL;
 	ft_printf("LARGE : 0x%X\n", tmpl);
 	while (tmpl)
 	{
 		i = -1;
 		while(++i < tmpl->n_elem)
+		{
 			ft_printf("0x%X - 0x%X : %lu octets\n", tmpl->mem[i], tmpl->mem[i]
 				+ tmpl->len[i], tmpl->len[i]);
+			total += tmpl->len[i];
+		}
 		tmpl = tmpl->next;
 	}
+	return (total);
 }
 
-void		show_alloc_mem(void)
+void					show_alloc_mem(void)
 {
-	t_malloc	*d;
+	unsigned long	total;
+	t_malloc		*d;
 
 	d = malloc_data();
-	pr_tiny(d);
-	pr_small(d);
-	pr_large(d);
+	total = 0UL;
+	total += pr_tiny(d);
+	total += pr_small(d);
+	total += pr_large(d);
+	ft_printf("Total : %llu octets\n", total);
 }
